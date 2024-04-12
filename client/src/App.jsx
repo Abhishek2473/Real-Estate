42763048319ab039b8889849d3f5bfc1dcbf9419
Abhishek Reddy <abhishekreddy2407@gmail.com>
2024-04-12 13:28:52 +0530

    Complete search and home pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
+import Search from './pages/Search'
 
 
 
@@ -22,7 +23,7 @@ export default function App() {
   <Route path='/sign-up' element={<SignUp/>}/>
   <Route path='/about' element={<About/>}/>
   <Route path='/listing/:listingId' element={<Listing/>}/>
-
+  <Route path='/search' element={<Search/>}/> 
   <Route  element={<PrivateRoute/>}>
   <Route path='/profile' element={<Profile/>}/>
   <Route path='/create-listing' element={<CreateListing/>}/>
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -112,10 +112,7 @@ export default function Listing() {
                 </p>
               )}
             </div>
-            <p className='text-slate-800'>
-              <span className='font-semibold text-black'>Description - </span>
-              {listing.description}
-            </p>
+
             <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
               <li className='flex items-center gap-1 whitespace-nowrap '>
                 <FaBed className='text-lg' />
@@ -138,6 +135,10 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
+            <p className='text-slate-800'>
+              <span className='font-semibold text-black'>Description - </span>
+              {listing.description}
+            </p>
             
             {currentUser && listing.userRef !== currentUser._id && !contact && (
               <button
