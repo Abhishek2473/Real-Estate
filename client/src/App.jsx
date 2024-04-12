a4e545bf68fb2c6f293e89ac32dd59d0e69fe64d
Abhishek Reddy <abhishekreddy2407@gmail.com>
2024-04-12 15:25:55 +0530

    ddd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import  {BrowserRouter,Routes,Route} from 'react-router-dom'
-
+import SignIn from './pages/SignIn'
 import Home from './pages/Home'
 import About from './pages/About'
-import Listing from './pages/Listing'
 import SignUp from './pages/SignUp'
-import SignIn from 'c:/Users/Abhishek Reddy/OneDrive/Desktop/Real-Estate Project/client/src/pages/SignIn';
 import Profile from './pages/Profile'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
-import UpdateListing from './pages/UpdateListing'
-import Search from './pages/Search'
 
 
 
@@ -23,14 +19,9 @@ export default function App() {
   <Route path='/sign-in' element={<SignIn/>}/>
   <Route path='/sign-up' element={<SignUp/>}/>
   <Route path='/about' element={<About/>}/>
-  <Route path='/listing/:listingId' element={<Listing/>}/>
-  <Route path='/search' element={<Search/>}/> 
   <Route  element={<PrivateRoute/>}>
   <Route path='/profile' element={<Profile/>}/>
   <Route path='/create-listing' element={<CreateListing/>}/>
-  <Route path='/update-listing/:listingId' element={<UpdateListing/>}/>
-
-  
   </Route>
   </Routes>
   </BrowserRouter>
