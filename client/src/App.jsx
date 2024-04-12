d301f7f2af67f8897d1c14f1d03b9466723c3597
Abhishek Reddy <abhishekreddy2407@gmail.com>
2024-04-12 15:55:52 +0530

    updated2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import  {BrowserRouter,Routes,Route} from 'react-router-dom'
-import SignIn from './pages/SignIn'
+import SignIn from '../src/pages/SignIn'
 import Home from './pages/Home'
 import About from './pages/About'
 import SignUp from './pages/SignUp'
